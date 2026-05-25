cd8c02d6bc72f5a64f4d8aeb530e11faa7f8558f
ajah <ajahajang00@gmail.com>
2026-05-25 17:13:21 +0200

    chore: create function to get all items

--- src/controllers/itemController.ts
+++ src/controllers/itemController.ts
@@ -1,20 +1,36 @@
-import Item from "../model/itemModel.js";
-import type { Request,Response } from "express";
-export  const create=async(req:Request,res:Response)=>{
-try{
-    const newItem=new Item(req.body)
-    const {id}=newItem
-    const exists=await Item.findOne({id})
-if(exists){
-    res.status(400).send('Item already exists')
+import Item from "../models/itemModel.js";
+import type { Request, Response } from "express";
+export const create = async (req: Request, res: Response) => {
+    try {
+        const newItem = new Item(req.body)
+        const { id } = newItem
+        const exists = await Item.findOne({ id })
+        if (exists) {
+            res.status(400).send('Item already exists')
+        }
+        const savedData = await newItem.save()
+        res.status(200).json(savedData)
+    }
+    catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(500).json({ errorMessage: error.message })
+        } else {
+            res.status(500).json({ errorMessage: String(error) })
+        }
+    }
 }
- const savedData=await newItem.save()
- res.status(200).json(savedData)
-}
-catch(error: unknown){
-  if (error instanceof Error) {
-    res.status(500).json({errorMessage: error.message})
-  } else {
-    res.status(500).json({errorMessage: String(error)})
-  }
-} }
+
+
+export const getAllItems = async (req: Request, res: Response) => {
+    try {
+        const itemData = await Item.find()
+     res.status(200).json(itemData)
+
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            res.status(500).send({ errorMessage: err.message })}
+          else {
+            res.status(500).send({ errorMessage: String(err) })
+        }
+    
+}}
