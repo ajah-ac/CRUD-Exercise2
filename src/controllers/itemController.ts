2af07bd10a79c7965befbfdc5a680514f90c387b
ajah <ajahajang00@gmail.com>
2026-05-25 17:24:57 +0200

    chore: get item by id

--- src/controllers/itemController.ts
+++ src/controllers/itemController.ts
@@ -6,7 +6,7 @@ export const create = async (req: Request, res: Response) => {
         const { id } = newItem
         const exists = await Item.findOne({ id })
         if (exists) {
-            res.status(400).send('Item already exists')
+          return res.status(400).send('Item already exists')
         }
         const savedData = await newItem.save()
         res.status(200).json(savedData)
@@ -26,6 +26,25 @@ export const getAllItems = async (req: Request, res: Response) => {
         const itemData = await Item.find()
      res.status(200).json(itemData)
 
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+          return   res.status(500).send({ errorMessage: err.message })}
+          else {
+            res.status(500).send({ errorMessage: String(err) })
+        }
+    
+}}
+
+
+export const getItemById=async (req: Request, res: Response) => {
+    try {
+       const id=req.params.id
+       const itemExist=await Item.findById(id)
+       if(!itemExist){
+        return res.status(404).send({messsage:'Item not found'})
+       }
+    
+         res.status(200).json(itemExist)
     } catch (err: unknown) {
         if (err instanceof Error) {
             res.status(500).send({ errorMessage: err.message })}
