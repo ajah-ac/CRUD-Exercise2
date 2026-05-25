50361839041e6b4b2efce3bb574937a443770732
ajah <ajahajang00@gmail.com>
2026-05-25 16:05:23 +0200

    chore: setup express server and connect to mongodb

---
+++ app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const MONGO_URI = process.env.MONGO_URI || '';
+mongoose.connect(MONGO_URI).then(()=>{
+    console.log('Connected to Mongodb');
+
+})
+.catch((err)=>{
+    console.log(`something went wrong ${err}`)
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log('Server is listening on port '+PORT);
+})
