efc5f4c4168b727bf207dfe23df4862b9be791fb
ajah <ajahajang00@gmail.com>
2026-05-25 17:25:14 +0200

    chore: get item by id routes

--- app.ts
+++ app.ts
@@ -7,6 +7,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use('/items',route)
+app.use('/items/:id',route)
+
 const MONGO_URI = process.env.MONGO_URI || '';
 mongoose.connect(MONGO_URI).then(()=>{
     console.log('Connected to Mongodb');
--- src/routes/itemRoutes.ts
+++ src/routes/itemRoutes.ts
@@ -1,6 +1,8 @@
 import express from 'express'
-import {create,getAllItems} from '../controllers/itemController.js'
+import {create,getAllItems, getItemById} from '../controllers/itemController.js'
 const router=express.Router()
 router.post('/', create)
 router.get('/',getAllItems)
+router.get('/:id',getItemById)
+
 export default router
