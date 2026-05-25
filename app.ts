0026917179bbf05f273715c960e983fe044af627
ajah <ajahajang00@gmail.com>
2026-05-25 17:00:46 +0200

    chore: use the post route

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import route from './src/routes/itemRoutes.js';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use('/',route)
 const MONGO_URI = process.env.MONGO_URI || '';
 mongoose.connect(MONGO_URI).then(()=>{
     console.log('Connected to Mongodb');
